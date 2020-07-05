831ab2f064880cdc80725fab8e7ea948d53fe114
Guille Figueroa <guille.figueroa9915@gmail.com>
2020-07-04 19:55:03 -0600

    MF17006 - CortoDos

---
+++ Corto2/target/generated-sources/annotations/com/progra2/persistencia/Usuario_.java
@@ -0,0 +1,16 @@
+package com.progra2.persistencia;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.4.v20190115-rNA", date="2020-07-04T19:43:19")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> correo;
+    public static volatile SingularAttribute<Usuario, String> contrasenia;
+    public static volatile SingularAttribute<Usuario, Long> id;
+    public static volatile SingularAttribute<Usuario, String> nombre;
+
+}
